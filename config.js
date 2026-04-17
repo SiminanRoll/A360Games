61c64663d5c6b5daa54336c70a0332305715cac7
PattyPoo <patricb@live.com>
2026-04-17 17:55:59 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.SPOT_THE_PHISH_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://gpqbamtgaqqsczzgucel.supabase.co',
+  supabaseAnonKey: 'sb_publishable_-WRoIozWWx6_vXWJtjXK5w_jAX2Y-qK',
   supabaseFeedbackFunctionUrl: '',
   siteLabel: 'Spot the Phish'
 };
